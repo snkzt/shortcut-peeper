8e6249e52471896b94afccf0d98e089967fb01f4
snkzt <snkzt@mercari.com>
2023-01-06 22:00:04 +0100

    Improved error: log internal errors and print user-facing errors

--- shortcuts.go
+++ shortcuts.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
+	"log"
 	"os"
 )
 
@@ -23,13 +24,13 @@ func createDirectory(fileStoragePath string) (string, error) {
 	case os.IsNotExist(err):
 		err = os.Mkdir(fileStoragePath, 0750)
 		if err != nil {
-			return "", fmt.Errorf("failed to create directory \".config\": %w", err)
+			return "", fmt.Errorf(`failed to create directory ".config": %w`, err)
 		}
 		fallthrough
 	case os.IsNotExist(errS):
 		err = os.Mkdir(speepDir, 0750)
 		if err != nil {
-			return "", fmt.Errorf("failed to create directory \".config/speep\": %w", err)
+			return "", fmt.Errorf(`failed to create directory ".config/speep": %w`, err)
 		}
 		return speepDir, nil
 	default:
@@ -46,7 +47,7 @@ func getEnvPath() (string, error) {
 	fileStoragePath += "/.config"
 	fileStoragePath, err := createDirectory(fileStoragePath)
 	if err != nil {
-		return "", fmt.Errorf("failed to retrieve env path: %w", err)
+		return "", fmt.Errorf(`failed to retrieve env path: %w`, err)
 	}
 
 	fileStoragePath += "/shortcuts.json"
@@ -59,23 +60,31 @@ func GetShortcuts() ([]Shortcut, error) {
 	// Check if the json file exists and create new one if it doesn't exist
 	fileStoragePath, err := getEnvPath()
 	if err != nil {
-		return nil, fmt.Errorf("failed to retrieve shortcut list: %w", err)
+		internalError := fmt.Errorf(`failed to retrieve shortcut list: %w`, err)
+		log.Println(internalError)
+		return nil, internalError
 	}
 
 	_, err = os.Open(fileStoragePath)
 	if err != nil {
-		return nil, fmt.Errorf("failed to open shortcut list: %w", err)
+		internalError := fmt.Errorf(`failed to open shortcut list: %w`, err)
+		log.Println(internalError)
+		return nil, internalError
 	}
 
 	fileBytes, err := os.ReadFile(fileStoragePath)
 	if err != nil {
-		return nil, fmt.Errorf("failed to read the shortcut list: %w", err)
+		internalError := fmt.Errorf(`failed to read the shortcut list: %w`, err)
+		log.Println(internalError)
+		return nil, internalError
 	}
 
 	// Set the decoded data of fileBytes to shortcuts struct
 	err = json.Unmarshal(fileBytes, &shortcuts)
 	if err != nil {
-		return nil, fmt.Errorf("failed to store byte data to shortcuts struct: %w", err)
+		internalError := fmt.Errorf(`failed to store byte data to shortcuts struct: %w`, err)
+		log.Println(internalError)
+		return nil, internalError
 	}
 	return shortcuts, nil
 }
@@ -86,7 +95,7 @@ func CheckNameDuplication(category string, name string) error {
 	for _, shortcut := range shortcutList {
 		if shortcut.Category == category {
 			if shortcut.Name == name {
-				return errors.New("already registered")
+				return errors.New(`already registered`)
 			}
 		}
 	}
@@ -96,19 +105,25 @@ func CheckNameDuplication(category string, name string) error {
 func SaveShortcuts(shortcuts []Shortcut) error {
 	shortcutBytes, err := json.Marshal(shortcuts)
 	if err != nil {
-		return fmt.Errorf("failed to store list as JSON : %w", err)
+		internalError := fmt.Errorf(`failed to store list as JSON : %w`, err)
+		log.Println(internalError)
+		return internalError
 	}
 
 	fileStoragePath, err := getEnvPath()
 	if err != nil {
-		return fmt.Errorf("failed to save shortcut list: %w", err)
+		internalError := fmt.Errorf(`failed to save shortcut list: %w`, err)
+		log.Println(internalError)
+		return internalError
 	}
 
 	// Write the new shortcut into the existing list
 	// and create one if no list exists
 	err = os.WriteFile(fileStoragePath, shortcutBytes, 0644)
 	if err != nil {
-		return fmt.Errorf("failed to save the new shortcut: %w", err)
+		internalError := fmt.Errorf(`failed to save the new shortcut: %w`, err)
+		log.Println(internalError)
+		return internalError
 	}
 	return nil
 }
@@ -117,12 +132,16 @@ func DeleteShortcuts() error {
 	// Remove shortcuts.json file
 	fileStoragePath, err := getEnvPath()
 	if err != nil {
-		return fmt.Errorf("failed to delete shortcut list: %w", err)
+		internalError := fmt.Errorf(`failed to delete shortcut list: %w`, err)
+		log.Println(internalError)
+		return internalError
 	}
 
 	err = os.Remove(fileStoragePath)
 	if err != nil {
-		return fmt.Errorf("failed to delete the existing list: %w", err)
+		internalError := fmt.Errorf(`failed to delete the existing list: %w`, err)
+		log.Println(internalError)
+		return internalError
 	}
 	return nil
 }
@@ -131,7 +150,9 @@ func DeleteShortcut(category string, name string) error {
 	// Get existing shortcut key list
 	shortcuts, err := GetShortcuts()
 	if err != nil {
-		return fmt.Errorf("failed to acquire existing list: %w", err)
+		internalError := fmt.Errorf(`failed to acquire existing list: %w`, err)
+		log.Println(internalError)
+		return internalError
 	}
 
 	for i, shortcut := range shortcuts {
@@ -141,7 +162,9 @@ func DeleteShortcut(category string, name string) error {
 					shortcuts = nil
 					err = SaveShortcuts(shortcuts)
 					if err != nil {
-						return fmt.Errorf("failed to save the list after removing an item: %w", err)
+						internalError := fmt.Errorf(`failed to save the list after removing an item: %w`, err)
+						log.Println(internalError)
+						return internalError
 					}
 					return nil
 				} else {
@@ -149,12 +172,16 @@ func DeleteShortcut(category string, name string) error {
 					shortcuts = shortcuts[:len(shortcuts)-1]
 					err = SaveShortcuts(shortcuts)
 					if err != nil {
-						return fmt.Errorf("failed to save the list after removing an item: %w", err)
+						internalError := fmt.Errorf(`failed to save the list after removing an item: %w`, err)
+						log.Println(internalError)
+						return internalError
 					}
 					return nil
 				}
 			}
 		}
 	}
-	return fmt.Errorf("the shortcut name %s for the category %s does not exist", name, category)
+	internalError := fmt.Errorf(`the shortcut name %s for the category %s does not exist`, name, category)
+	log.Println(internalError)
+	return internalError
 }
